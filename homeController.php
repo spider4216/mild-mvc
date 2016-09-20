489c36a487a15abd761df9bee59376479821c9f5
Jackson1911 <7witcher791@mail.ru>
2016-09-20 12:40:32 +0600

    Add files via upload

---
+++ homeController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace controllers;
+use system\CView;
+use system\SystemController;
+use system\App;
+
+/**
+ * Class homeController
+ * @package controllers
+ * @author farZa
+ *
+ * Actions: action<Name> (camelCase)
+ * Controllers: <Name>Controller (camelCase)
+ */
+class homeController extends SystemController
+{
+	public function actionIndex()
+	{
+		CView::render('index');
+	}
+}
